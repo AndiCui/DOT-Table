553605531acde8b7173826d0badf45ddbe82d731
AndiCui <andy990525@me.com>
2016-08-27 21:54:13 +0200

    scrolling and snipping text
    column-wide scrolling settings, text stay in the cell

--- DOT_Table.py
+++ DOT_Table.py
@@ -4,6 +4,10 @@ total_rows=3
 
 class Table():
     def __init__(self, *col_lenths, seperation_char=" ", gap_size=1):
+        lcd.clear()
+        self.cells = [[],[],[]]
+        self.scrollable_cols = []
+        self.offset = 0
         if len(seperation_char) > gap_size:
             raise ValueError('Seperation symbols cannot be longer than the gap.')
             pass
@@ -14,6 +18,9 @@ class Table():
         for col in range(len(col_lenths)):
             self.starting_point.append(self.starting_point[col] + self.col_lenths[col] + gap_size)
             self.seperation_char_location.append(self.starting_point[col] + self.col_lenths[col])
+            self.scrollable_cols.append(False)
+        for row in range(total_rows):
+            self.cells[row] = [""] * len(col_lenths)
         del self.seperation_char_location[-1]
         self.draw_seperation_char()
 
@@ -23,6 +30,32 @@ class Table():
                 lcd.set_cursor_position(self.seperation_char_location[loc], row)
                 lcd.write(self.seperation_char)
 
-    def write_cell(self, string, col, row):
+    def set_cell(self, string, col, row):
+        self.cells[row][col] = string
+
+    def _write_cell(self, col, row):
+        string = self.cells[row][col]
         lcd.set_cursor_position(self.starting_point[col], row)
-        lcd.write(string)
+        lcd.write(string[0:self.col_lenths[col]])
+
+    def _write_cell_scrollable(self, col, row, offset):
+        string = self.cells[row][col]
+        if len(string)<=self.col_lenths[col]:
+            offset=0
+        else:
+            offset%=(len(string))
+        lcd.set_cursor_position(self.starting_point[col], row)
+        lcd.write(string[offset:self.col_lenths[col]+offset])
+
+    def update(self):
+        for col in range(len(self.col_lenths)):
+            if self.scrollable_cols[col] == True:
+                for row in range(total_rows):
+                    self._write_cell_scrollable(col,row,self.offset)
+            else:
+                for row in range(total_rows):
+                    self._write_cell(col,row)
+
+    def scroll(self):
+        self.offset+=1
+        self.update()
